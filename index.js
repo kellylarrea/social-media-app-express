59b63c34a9022d9db7ea187961a75fc0d2f5c48c
gsdkira <larreakelly@gmail.com>
2022-04-04 20:33:33 -0400

    initialized server

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+//set up starting path, routes
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://larreakelly:gundam123@cluster0.rv2ij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false); deprecated found on stackoverflow
