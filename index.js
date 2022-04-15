21e12719f21052075a432d8028a60cdad71d8790
gsdkira <larreakelly@gmail.com>
2022-04-15 13:16:44 -0400

    added dotenv for sensitive info

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config()
 
 //set up starting path, routes
 
@@ -15,11 +17,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-
-const CONNECTION_URL = "mongodb+srv://larreakelly:gundam123@cluster0.rv2ij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
